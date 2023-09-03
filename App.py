010769b378d215706b6ecd477d8fdc11dfb74028
Chrisathan12 <chris.athan12@gmail.com>
2023-09-03 12:30:29 -0500

    implementacion de ventana que abre resultados

--- App.py
+++ App.py
@@ -1,8 +1,11 @@
+from fileinput import fileno
+from inspect import getfile
 from tkinter import *
 from tkinter import messagebox
 from tkinter import filedialog
 from tkinter import Tk, Label, Button, Frame,  messagebox, filedialog, ttk, Scrollbar, VERTICAL, HORIZONTAL
 from PIL import ImageTk
+import pandas as pd
 
 # VARIABLES GLOBALES
 csvFile = ''
@@ -55,7 +58,7 @@ def resWindow():
         resWindow = Toplevel(root)
         resWindow.title('ReVin - Resultados')
         resWindow.geometry('600x400')
-        resWindow.config(bg='black')
+        resWindow.config(bg='lightblue')
         resWindow.protocol("WM_DELETE_WINDOW", lambda: on_closing(resWindow))
         resWindow.iconbitmap('Images/appWindowIcon.ico')
         
@@ -67,9 +70,9 @@ def resWindow():
         resWindow.rowconfigure(1, weight= 1)
         
         # ELEMENTOS
-        frame1 = Frame(resWindow, bg='gray26')
+        frame1 = Frame(resWindow, bg='lightblue')
         frame1.grid(column=0,row=0,sticky='nsew')
-        frame2 = Frame(resWindow, bg='gray26')
+        frame2 = Frame(resWindow, bg='lightblue')
         frame2.grid(column=0,row=1,sticky='nsew')
         
         frame1.columnconfigure(0, weight = 1)
@@ -85,6 +88,44 @@ def resWindow():
         
         frame2.columnconfigure(3, weight = 2)
         frame2.rowconfigure(0, weight= 1)
+
+        def abrir_archivo():
+            #archivo = '/predicted.csv'
+            archivo = filedialog.askopenfilename(initialdir ='/', 
+											title='Selecione archivo', 
+											filetype=((' files', '*.csv*'),('All files', '*.*')))
+            indica['text'] = archivo
+
+        def datos_excel():
+            
+            datos_obtenidos = indica['text']
+            try:
+                archivocsv = r'{}'.format(datos_obtenidos)
+                df = pd.read_csv(archivocsv)
+
+            except ValueError:
+                messagebox.showerror('Informacion','Formato incorrecto')
+                return None
+
+            except FileNotFoundError:
+                messagebox.showerror('Informacion','El archivo está \n malogrado')
+                return None
+
+            Limpiar()
+
+            tabla['column'] = list(df.columns)
+            tabla['show'] = "headings"
+
+            for columna in tabla['column']:
+                tabla.heading(columna, text= columna)
+
+            df_fila = df.to_numpy().tolist()
+            for fila in df_fila:
+                tabla.insert('','end',values =fila)
+
+        def Limpiar():
+            tabla.delete(*tabla.get_children())
+
         tabla = ttk.Treeview(frame1 , height=10)
         tabla.grid(column=0, row=0, sticky='nsew')
         
@@ -98,63 +139,25 @@ def resWindow():
         
         estilo = ttk.Style(frame1)
         estilo.theme_use('clam') #  ('clam', 'alt', 'default', 'classic')
-        estilo.configure(".",font= ('Arial', 14), foreground='red2')
-        estilo.configure("Treeview", font= ('Helvetica', 12), foreground='black',  background='white')
+        estilo.configure(".",font= ('Arial', 14), foreground='black')
+        estilo.configure("Treeview", font= ('Javanese Text', 12), foreground='black',  background='white')
         estilo.map('Treeview',background=[('selected', 'green2')], foreground=[('selected','black')] )
         
         
         boton1 = Button(frame2, text= 'Abrir', bg='green2', command= abrir_archivo)
         boton1.grid(column = 0, row = 0, sticky='nsew', padx=10, pady=10)
         
-        boton2 = Button(frame2, text= 'Mostrar', bg='magenta', command= datos_excel)
+        boton2 = Button(frame2, text= 'Mostrar', bg='springgreen', command= datos_excel)
         boton2.grid(column = 1, row = 0, sticky='nsew', padx=10, pady=10)
         
-        boton3 = Button(frame2, text= 'Limpiar', bg='red', command= Limpiar)
-        boton3.grid(column = 2, row = 0, sticky='nsew', padx=10, pady=10)
+        #boton3 = Button(frame2, text= 'Limpiar', bg='red', command= Limpiar)
+        #boton3.grid(column = 2, row = 0, sticky='nsew', padx=10, pady=10)
         
         
-        indica = Label(frame2, fg= 'white', bg='gray26', text= 'Ubicación del archivo', font= ('Arial',10,'bold') )
+        indica = Label(frame2, fg= 'white', bg='lightblue', text= 'Ubicación del archivo', font= ('Javanese Text',10,'bold') )
         indica.grid(column=3, row = 0)
 
-def abrir_archivo():
-	
-    # archivo = archivo de predicciones
 
-	indica['text'] = archivo
-
-def datos_excel():
-
-	datos_obtenidos = indica['text']
-	try:
-		archivoexcel = r'{}'.format(datos_obtenidos)
-		
-
-		df = pd.read_excel(archivoexcel)
-
-	except ValueError:
-		messagebox.showerror('Informacion', 'Formato incorrecto')
-		return None
-
-	except FileNotFoundError:
-		messagebox.showerror('Informacion', 'El archivo esta \n malogrado')
-		return None
-
-	Limpiar()
-
-	tabla['column'] = list(df.columns)
-	tabla['show'] = "headings"  #encabezado
-     
-
-	for columna in tabla['column']:
-		tabla.heading(columna, text= columna)
-	
-
-	df_fila = df.to_numpy().tolist()
-	for fila in df_fila:
-		tabla.insert('', 'end', values =fila)
-
-def Limpiar():
-	tabla.delete(*tabla.get_children())
 
 #   Confirmacion de cierre de ventana
 def on_closing(window):
